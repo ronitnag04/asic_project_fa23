ee7f8721f21b90a9fffceaeabb253c8484bdce91
ronitnag04 <ronitnag04@gmail.com>
2023-12-19 16:41:18 -0600

    Added R-Type Test Vector Generator

--- tests/ALUTestGen.py
+++ tests/ALUTestGen.py
@@ -78,6 +78,15 @@ functs_itype = \
 functs_rtype = \
 {
     "ADD":     ("000", "0", lambda a,b: a+b, lambda a: a, lambda b: b),
+    "SUB":     ("000", "1", lambda a,b: a-b, lambda a: a, lambda b: b),
+    "SLL":     ("001", "0", lambda a,b: a<<b, lambda a: a, lambda b: b),
+    "SLT":     ("010", "0", lambda a,b: (lambda:0, lambda:1)[comp(a,b)](), lambda a: a, lambda b: b),
+    "SLTU":    ("011", "0", lambda a,b: (lambda:0, lambda:1)[a < b](), lambda a: a, lambda b: b),
+    "XOR":     ("100", "0", lambda a,b: a^b, lambda a: a, lambda b: b),
+    "SRL":     ("101", "0", lambda a,b: a>>b, lambda a: a, lambda b: b),
+    "SRA":     ("101", "1", lambda a,b: sra(a,b), lambda a: a, lambda b: b),
+    "OR":      ("110", "0", lambda a,b: a|b, lambda a: a, lambda b: b),
+    "AND":     ("111", "0", lambda a,b: a&b, lambda a: a, lambda b: b),
 }
 
 random.seed(os.urandom(32))
