43803a519749f7e6e13c42bd69c763ed7f7bbd5c
ronitnag04 <ronitnag04@gmail.com>
2023-12-19 17:37:30 -0600

    Add always block

--- src/ALUdec.v
+++ src/ALUdec.v
@@ -17,45 +17,47 @@ module ALUdec(
 );
 
   // Implement your ALU decoder here, then delete this comment
-case (opcode)
+always @(*) begin
+  case (opcode)
 
-  `OPC_ARI_ITYPE,
-  `OPC_ARI_RTYPE: begin
-    case (funct)
-      `FNC_ADD_SUB : begin
-        case (add_rshift_type)
-          `FNC2_ADD: ALUop <= `ALU_ADD;
-          `FNC2_SUB: ALUop <= `ALU_SUB;
-          default: `ALUop <= `ALU_XXX;
-        endcase
-      end
-      `FNC_SLL     : ALUop <= `ALU_SLL;
-      `FNC_SLT     : ALUop <= `ALU_SLT;
-      `FNC_SLTU    : ALUop <= `ALU_SLTU;
-      `FNC_XOR     : ALUop <= `ALU_XOR;
-      `FNC_OR      : ALUop <= `ALU_OR;
-      `FNC_AND     : ALUop <= `ALU_AND;
-      `FNC_SRL_SRA : begin
-        case (add_rshift_type)
-          `FNC2_SRL: ALUop <= `ALU_SRL;
-          `FNC2_SRA: ALUop <= `ALU_SRA;
-          default  : ALUop <= `ALU_XXX;
-        endcase
-      end;
-      default      : ALUop <= `ALU_XXX;
-    endcase
-  end
+    `OPC_ARI_ITYPE,
+    `OPC_ARI_RTYPE: begin
+      case (funct)
+        `FNC_ADD_SUB : begin
+          case (add_rshift_type)
+            `FNC2_ADD: ALUop <= `ALU_ADD;
+            `FNC2_SUB: ALUop <= `ALU_SUB;
+            default: `ALUop <= `ALU_XXX;
+          endcase
+        end
+        `FNC_SLL     : ALUop <= `ALU_SLL;
+        `FNC_SLT     : ALUop <= `ALU_SLT;
+        `FNC_SLTU    : ALUop <= `ALU_SLTU;
+        `FNC_XOR     : ALUop <= `ALU_XOR;
+        `FNC_OR      : ALUop <= `ALU_OR;
+        `FNC_AND     : ALUop <= `ALU_AND;
+        `FNC_SRL_SRA : begin
+          case (add_rshift_type)
+            `FNC2_SRL: ALUop <= `ALU_SRL;
+            `FNC2_SRA: ALUop <= `ALU_SRA;
+            default  : ALUop <= `ALU_XXX;
+          endcase
+        end;
+        default      : ALUop <= `ALU_XXX;
+      endcase
+    end
 
-  `OPC_LUI         : ALUop <= `ALU_COPY_B;
+    `OPC_LUI         : ALUop <= `ALU_COPY_B;
 
-  `OPC_JAL,
-  `OPC_JALR,
-  `OPC_BRANCH,
-  `OPC_LOAD,
-  `OPC_STORE,
-  `OPC_AUIPC       : ALUop <= `ALU_ADD;
+    `OPC_JAL,
+    `OPC_JALR,
+    `OPC_BRANCH,
+    `OPC_LOAD,
+    `OPC_STORE,
+    `OPC_AUIPC       : ALUop <= `ALU_ADD;
 
-  default : ALUop <= `ALU_XXX;
-endcase
+    default : ALUop <= `ALU_XXX;
+  endcase
+end
 
 endmodule
