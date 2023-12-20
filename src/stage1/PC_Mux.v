5ef58577e0cbd1fa3e94afae2b50d3be273ac434
ronitnag04 <ronitnag04@gmail.com>
2023-12-20 16:38:05 -0600

    Added PC Modules

---
+++ src/stage1/PC_Mux.v
@@ -0,0 +1,24 @@
+// Module: PC_Mux.v
+// Desc:   Mux for next PC value
+// Inputs: 
+//    PC_4: 32-bit Previous PC + 4;
+//    ALU_Out: 32-bit Value from ALU in Stage 2
+//    PC_Sel: Select line
+// 						
+// Outputs:
+//    PC_Mux_Out: 32-bit PC value for next state to connect to PC_In
+
+`include "stage1_control.vh"
+
+module PC_Mux(
+    input [31:0] PC_4, ALU_Out,
+    input PC_Sel,
+
+    output [31:0] PC_Mux_Out,
+);
+
+always @(*) begin
+    assign PC_Mux_Out = (PC_Sel == `PC_SEL_PC_4) ? PC_4 : ALU_Out;
+end
+
+endmodule
---
+++ src/stage1/stage1_control.vh
@@ -0,0 +1,8 @@
+`ifndef STAGE1_CONTROL
+`define STAGE1_CONTROL
+
+// PC_Mux Control
+`define PC_SEL_PC_4      1'b0
+`define PC_SEL_ALU_OUT   1'b1
+
+`endif //STAGE1_CONTROL
