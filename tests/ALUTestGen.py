5a70d47dd5c5aad39c32c1ba1a97f7203c0e9d6a
ronitnag04 <ronitnag04@gmail.com>
2023-12-19 23:01:49 -0600

    Fix test vector generation

--- tests/ALUTestGen.py
+++ tests/ALUTestGen.py
@@ -79,12 +79,12 @@ functs_rtype = \
 {
     "ADD":     ("000", "0", lambda a,b: a+b, lambda a: a, lambda b: b),
     "SUB":     ("000", "1", lambda a,b: a-b, lambda a: a, lambda b: b),
-    "SLL":     ("001", "0", lambda a,b: a<<b, lambda a: a, lambda b: b),
+    "SLL":     ("001", "0", lambda a,b: a<<b, lambda a: a, lambda b: b&0x1f),
     "SLT":     ("010", "0", lambda a,b: (lambda:0, lambda:1)[comp(a,b)](), lambda a: a, lambda b: b),
     "SLTU":    ("011", "0", lambda a,b: (lambda:0, lambda:1)[a < b](), lambda a: a, lambda b: b),
     "XOR":     ("100", "0", lambda a,b: a^b, lambda a: a, lambda b: b),
-    "SRL":     ("101", "0", lambda a,b: a>>b, lambda a: a, lambda b: b),
-    "SRA":     ("101", "1", lambda a,b: sra(a,b), lambda a: a, lambda b: b),
+    "SRL":     ("101", "0", lambda a,b: a>>b, lambda a: a, lambda b: b&0x1f),
+    "SRA":     ("101", "1", lambda a,b: sra(a,b), lambda a: a, lambda b: b&0x1f),
     "OR":      ("110", "0", lambda a,b: a|b, lambda a: a, lambda b: b),
     "AND":     ("111", "0", lambda a,b: a&b, lambda a: a, lambda b: b),
 }
