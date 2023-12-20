75b8e956a7820e2af8d6ccdb9e7e3dd1dd8f9f72
ronitnag04 <ronitnag04@gmail.com>
2023-12-19 23:41:01 -0600

    Fix sub test generator function

--- tests/ALUTestGen.py
+++ tests/ALUTestGen.py
@@ -78,7 +78,7 @@ functs_itype = \
 functs_rtype = \
 {
     "ADD":     ("000", "0", lambda a,b: a+b, lambda a: a, lambda b: b),
-    "SUB":     ("000", "1", lambda a,b: a-b, lambda a: a, lambda b: b),
+    "SUB":     ("000", "1", lambda a,b: (a-b)&0xffff_ffff, lambda a: a, lambda b: b),
     "SLL":     ("001", "0", lambda a,b: a<<b, lambda a: a, lambda b: b&0x1f),
     "SLT":     ("010", "0", lambda a,b: (lambda:0, lambda:1)[comp(a,b)](), lambda a: a, lambda b: b),
     "SLTU":    ("011", "0", lambda a,b: (lambda:0, lambda:1)[a < b](), lambda a: a, lambda b: b),
