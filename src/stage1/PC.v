faf627891beca47a046f2cc834fdde255eb1b930
ronitnag04 <ronitnag04@gmail.com>
2023-12-21 14:25:39 -0600

    Condensed PC modules

--- src/stage1/PC.v
+++ src/stage1/PC.v
@@ -1,15 +1,18 @@
 // Module: PC.v
-// Desc:   PC unit module
+// Desc:   Program Counter w/ synchronous reset, stall, and ALU load
 // Inputs: 
 //    ALU_Out: 32-bit value from ALU_Out in stage2
 //    clk: Clock line
 //    reset: Reset line
 //
 //    stall: Stall counter
-//    PC_Sel: Select next PC
+//    PC_Sel: Select next PC (0: PC + 4, 1: ALU_Out)
 // 						
 // Outputs:
 //    PC_Out: 32-bit PC value for current state
+
+`include "stage1/stage1_control.vh"
+`include "const.vh"
 
 module PC (
     input [31:0] ALU_Out,
@@ -18,27 +21,15 @@ module PC (
     input stall,
     input PC_Sel,
 
-    output [31:0] PC_Out
+    output reg [31:0] PC_Out
 );
 
-wire [31:0] PC_4, PC_Mux_Out;
-assign PC_4 = PC_Out + 4;
 
-PC_Reg PCREG (
-    .PC_In(PC_Mux_Out),
-    .clk(clk),
-    .reset(reset),
-    .stall(stall),
+always @(posedge clk) begin
+    if (reset) PC_Out <= `PC_RESET;
+    else if (stall) PC_Out <= PC_Out;
+    else PC_Out <= (PC_Sel == `PC_SEL_PC_4) ? PC_Out + 4 : ALU_Out;
+end
 
-    .PC_Out(PC_Out)
-); 
-
-PC_Mux PCMUX (
-    .PC_4(PC_4), 
-    .ALU_Out(ALU_Out),
-    .PC_Sel(PC_Sel),
-
-    .PC_Mux_Out(PC_Mux_Out)
-);
 
 endmodule
