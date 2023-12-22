a5cea2a0a4886b2f490d2436f2b0676f810a44a3
ronitnag04 <ronitnag04@gmail.com>
2023-12-22 00:39:48 -0600

    Wrote Two's Complement and Unisgned binary convertors

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,14 @@
+def twos_bin(x, width):
+    assert -(2**(width-1)) <= x and x <= (2**(width-1)) - 1, f'{x} cannot be represented in {width} bits'
+
+    if x < 0:
+        x = 2**width + x
+    
+    b = format(x, f'0{width}b')
+    return b
+
+
 def bin(x, width):
-    if x < 0: x = (~x) + 1
-    return ''.join([(x & (1 << i)) and '1' or '0' for i in range(width-1, -1, -1)])
+    assert 0 <= x and x <= (2**(width)) - 1, f'{x} cannot be represented in {width} bits'
+    b = format(x, f'0{width}b')
+    return b
