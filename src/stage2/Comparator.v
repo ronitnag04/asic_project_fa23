70caadee310489d93b1b1bdd53adbfdfb48da3cb
ronitnag04 <ronitnag04@gmail.com>
2023-12-23 01:31:00 -0600

    Fix error

--- src/stage2/Comparator.v
+++ src/stage2/Comparator.v
@@ -23,6 +23,6 @@ assign eq = (rs1d == rs2d) ? 1'b1 : 1'b0;
 wire [31:0] comp_rs1d, comp_rs2d;
 assign comp_rs1d = (s == 1'b1) ? $signed(rs1d) : $unsigned(rs1d);
 assign comp_rs2d = (s == 1'b1) ? $signed(rs2d) : $unsigned(rs2d);
-assign lt = (comp_rs1d < comp_rs2d) ? 1'b1 < 1'b0;
+assign lt = (comp_rs1d < comp_rs2d) ? 1'b1 : 1'b0;
 
 endmodule
