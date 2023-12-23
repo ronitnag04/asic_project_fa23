8b3b196b84d859bb067d6db19727460b638834b7
ronitnag04 <ronitnag04@gmail.com>
2023-12-23 01:45:30 -0600

    Comparator V2

--- src/stage2/Comparator.v
+++ src/stage2/Comparator.v
@@ -20,9 +20,8 @@ module Comparator (
 );
 
 assign eq = (rs1d == rs2d) ? 1'b1 : 1'b0;
-wire [31:0] comp_rs1d, comp_rs2d;
-assign comp_rs1d = (s == 1'b1) ? $signed(rs1d) : $unsigned(rs1d);
-assign comp_rs2d = (s == 1'b1) ? $signed(rs2d) : $unsigned(rs2d);
-assign lt = (comp_rs1d < comp_rs2d) ? 1'b1 : 1'b0;
+assign lt = (s == 1'b1) ?
+                (($signed(rs1d) < $signed(rs2d)) ? 1'b1 : 1'b0) :
+                ((rs1d < rs2d) ? 1'b1 : 1'b0);
 
 endmodule
